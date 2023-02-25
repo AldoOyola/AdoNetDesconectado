a60b4399e1681ed2230bad6e3efb668dc7bcfe64
Aldo Oyola <alexander.oyola02@gmail.com>
2023-02-24 22:01:21 -0500

    AdoNetDesconectado

---
+++ frmTipoClienteEdit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDesconectado
+{
+    public partial class frmTipoClienteEdit : Form
+    {
+        DataRow fila;
+        public frmTipoClienteEdit(DataRow filaEditar = null)
+        {
+            InitializeComponent();
+
+            if(filaEditar != null)
+            {
+                this.fila = filaEditar;
+                this.Text = "Editar registro";
+                MostrarDatos();
+            }          
+        }
+        private void AceptarCambios(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("Debe de ingresar un nombre valido","Sistemas", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+        private void MostrarDatos()
+        {
+            txtNombre.Text = this.fila["Nombre"].ToString();
+        }
+    }
+}
